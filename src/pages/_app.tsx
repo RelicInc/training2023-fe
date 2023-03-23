70a1845525a5da57f54d4e56147c9e3945cb91a8
eikiyuya <yuya.eiki@relic.co.jp>
2023-03-23 22:34:14 +0900

    [update] topページ大枠のレイアウト作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { MetaInfo } from "@/components/common/meta-info";
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <MetaInfo />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+export default App;
