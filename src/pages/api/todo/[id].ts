d7b14c5fcb508d19c5f4f310cfae240b72a57fd7
mizukiEndo-relic <mizuki.endo@relic.co.jp>
2023-04-12 21:07:41 +0900

    Swagger を設定

--- src/pages/api/todo/[id].ts
+++ src/pages/api/todo/[id].ts
@@ -15,7 +15,7 @@ type ErrorResponse = {
   message: string;
 };
 
-type Method = 'GET' | 'PUT' | 'DELETE';
+type Method = 'GET' | 'PUT' | 'DELETE' | 'OPTIONS';
 const prisma = new PrismaClient();
 export default async function handler(
   req: NextApiRequest,
@@ -27,6 +27,20 @@ export default async function handler(
     query: { id },
     method,
   } = req;
+
+  /*
+    Swagger ではプリフライトリクエストが飛ぶので、
+    Swagger でレスポンスを受け取る場合に必要
+  */
+  if (method === 'OPTIONS') {
+    return res
+      .status(200)
+      .setHeader("Access-Control-Allow-Origin", "*") 
+      .setHeader("Access-Control-Allow-Methods", "*")
+      .setHeader("Access-Control-Allow-Headers", "*")
+      .end()
+  }
+
   if (typeof id !== 'string') return;
   const task = await prisma.task.findFirst({ where: { id: +id } });
   if (!task) return res.status(404).json({ message: 'Not Found' });
@@ -39,7 +53,12 @@ export default async function handler(
         where: { id: +id },
         data: req.body,
       });
-      return res.status(200).json({ task });
+      return res
+        .status(200)
+        .setHeader("Access-Control-Allow-Origin", "*")  // Swagger でレスポンスを受け取る場合に必要
+        .setHeader("Access-Control-Allow-Methods", "*") // Swagger でレスポンスを受け取る場合に必要
+        .setHeader("Access-Control-Allow-Headers", "*") // Swagger でレスポンスを受け取る場合に必要
+        .json({ task });
     }
     case 'DELETE': {
       const task = await prisma.task.delete({ where: { id: +id } });
@@ -47,3 +66,4 @@ export default async function handler(
     }
   }
 }
+
