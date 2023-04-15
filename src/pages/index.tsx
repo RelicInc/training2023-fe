9afd3ecac209c7ec79dca6619c40959452cc4775
YOSHIKIOKAJIMA <yoshiki.okajima@relic.co.jp>
2023-04-16 05:54:15 +0900

    コンポーネントが分かれていない状態に

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,24 @@ import { NextPage } from 'next';
 import { useEffect, useState } from 'react';
 import { getAllTasks } from '@/utils/get-all-tasks';
 import { Task } from '@prisma/client';
-import { Layout } from '@/components/layout';
-import { TaskForm } from '@/components/task-form';
-import { TaskList } from '@/components/task-list';
+import { createTask } from '@/utils/create-task';
 
 const Index: NextPage = () => {
   const [allTaskList, setAllTaskList] = useState<Task[]>([]);
+  const [todoText, setTodoText] = useState('');
+
+  const onChangeTodoText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoText(event.currentTarget.value);
+  };
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = await createTask(todoText);
+    if (data) {
+      setAllTaskList([data.task, ...allTaskList]);
+      setTodoText('');
+    }
+  };
 
   const initializeTaskList = async () => {
     const data = await getAllTasks();
@@ -20,13 +32,56 @@ const Index: NextPage = () => {
     initializeTaskList();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
   return (
-    <Layout>
-      <TaskForm allTaskList={allTaskList} setAllTaskList={setAllTaskList} />
-      <TaskList allTaskList={allTaskList} />
-      {allTaskList.length === 0 && (
-        <div className="no_task">予定はないようです。</div>
-      )}
-    </Layout>
+    <main className="main">
+      <h1 className="heading">React研修：TODOアプリ</h1>
+      <div className="container">
+        <form className="form_container" onSubmit={onSubmit}>
+          <input
+            id="task-input-area"
+            className="input"
+            type="text"
+            placeholder="今日は何をしますか...?"
+            value={todoText}
+            onChange={onChangeTodoText}
+          />
+          <button type="submit" className="submit_btn" disabled={!todoText}>
+            作成
+          </button>
+        </form>
+        <ul className="task_ui">
+          {allTaskList.map((task) => (
+            <li key={task.id} className="task_li">
+              <input
+                type="checkbox"
+                className="check_box"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  console.log(`id: ${task.id}`);
+                  console.log(`完了: ${event.target.checked}`);
+                }}
+              />
+              <div className="task_text">{task.value}</div>
+              <button
+                type="button"
+                className="edit_task"
+                onClick={() => console.log(`編集：${task.id}`)}
+              >
+                編集
+              </button>
+              <button
+                type="button"
+                className="remove_task"
+                onClick={() => console.log(`削除：${task.id}`)}
+              >
+                削除
+              </button>
+            </li>
+          ))}
+        </ul>
+        {allTaskList.length === 0 && (
+          <div className="no_task">予定はないようです。</div>
+        )}
+      </div>
+    </main>
   );
 };
 
