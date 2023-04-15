b9dee554cc2ffaa364318f4224d8e4e6dd04cc6c
YOSHIKIOKAJIMA <yoshiki.okajima@relic.co.jp>
2023-04-15 21:05:11 +0900

    createTaskRow()で呼び出される関数群のイベントハンドラを正常な動作に修正

--- dom-training/script.js
+++ dom-training/script.js
@@ -1,13 +1,13 @@
-const form = document.getElementById("todo-form");
-const registerField = document.getElementById("todo-register-field");
-const submitButton = document.getElementById("submit-button");
-const taskList = document.getElementById("task-list");
-const emptyPlaceholder = document.getElementById("empty-placeholder");
+const form = document.getElementById('todo-form');
+const registerField = document.getElementById('todo-register-field');
+const submitButton = document.getElementById('submit-button');
+const taskList = document.getElementById('task-list');
+const emptyPlaceholder = document.getElementById('empty-placeholder');
 
 // submitイベント時に処理を実行する関数を割り当てる
-form.addEventListener("submit", onSubmitPostTask);
+form.addEventListener('submit', onSubmitPostTask);
 
-registerField.addEventListener("input", switchSubmitButtonDisability);
+registerField.addEventListener('input', switchSubmitButtonDisability);
 
 getInitialTasks().then(function (data) {
   // 取得したTODOのdataを画面に表示する処理を実行
@@ -28,7 +28,7 @@ async function onSubmitPostTask(event) {
       value: taskName,
     });
     addNewTask(data.task.id, data.task.value);
-    event.target.elements.task_input_area.value = "";
+    event.target.elements.task_input_area.value = '';
   } catch (err) {
     if (axios.isAxiosError(err)) {
       console.error(err.message);
@@ -42,7 +42,7 @@ async function onSubmitPostTask(event) {
  * 送信ボタンのdisabledのつけ外しをする
  */
 function switchSubmitButtonDisability(event) {
-  submitButton.disabled = event.target.value === "";
+  submitButton.disabled = event.target.value === '';
 }
 
 /**
@@ -60,9 +60,9 @@ function addNewTask(taskId, taskName) {
  * TODO要素を作成する関数
  */
 function createTaskRow(taskId, taskName) {
-  const taskListItem = document.createElement("li");
-  taskListItem.setAttribute("id", taskId);
-  taskListItem.classList.add("task_li");
+  const taskListItem = document.createElement('li');
+  taskListItem.setAttribute('id', taskId);
+  taskListItem.classList.add('task_li');
   const checkbox = createTaskCheckbox(taskId);
   const taskText = cerateTaskText(taskName);
   const editButton = createEditButton(taskId, taskName);
@@ -75,39 +75,40 @@ function createTaskRow(taskId, taskName) {
  * createTaskRow()で呼び出される関数群
  */
 function createTaskCheckbox(taskId) {
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.classList.add("check_box");
-  checkbox.addEventListener("change", function () {
-    updateTaskStatus(taskId);
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.classList.add('check_box');
+  checkbox.addEventListener('change', function () {
+    console.log('id: ' + taskId);
+    console.log('完了: ' + this.checked);
   });
   return checkbox;
 }
 function cerateTaskText(taskName) {
-  const taskText = document.createElement("p");
-  taskText.classList.add("task_text");
+  const taskText = document.createElement('p');
+  taskText.classList.add('task_text');
   taskText.textContent = taskName;
   return taskText;
 }
 
 function createEditButton(taskId, taskName) {
-  const button = document.createElement("button");
-  button.innerText = "編集";
-  button.type = "button";
-  button.classList.add("edit_task");
-  button.addEventListener("click", function () {
-    onClickEditButton(taskId, taskName);
+  const button = document.createElement('button');
+  button.innerText = '編集';
+  button.type = 'button';
+  button.classList.add('edit_task');
+  button.addEventListener('click', function () {
+    console.log('編集：' + taskId);
   });
   return button;
 }
 
 function createDeleteButton(taskId) {
-  const button = document.createElement("button");
-  button.innerText = "削除";
-  button.type = "button";
-  button.classList.add("remove_task");
-  button.addEventListener("click", function () {
-    onClickDeleteButton(taskId);
+  const button = document.createElement('button');
+  button.innerText = '削除';
+  button.type = 'button';
+  button.classList.add('remove_task');
+  button.addEventListener('click', function () {
+    console.log('削除：' + taskId);
   });
   return button;
 }
